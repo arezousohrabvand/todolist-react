5d44389a5dc2d1912ab30466a0dca4945c8dbf8e
arezousohrabvand <200387480@student.georgianc.on.ca>
2021-08-17 13:16:07 -0400

    update

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import TodoList from './components/TodoList';
 
 function App() {
   return (
+ 
     <div className="todo-app">
+      <span class="text">React App</span>
       <h1>Todo App</h1>
+      
       <TodoList />
     </div>
   );
